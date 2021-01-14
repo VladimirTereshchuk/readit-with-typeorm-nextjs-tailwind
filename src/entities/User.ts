b46a2a535eb38fc39e0c8dba4b4671bf989f44d9
VladimirTereshchuk <vofca1974@gmail.com>
2021-01-14 21:25:42 +0200

    added login page, axios,cors,font-famile

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -19,18 +19,18 @@ export default class User extends Entity {
   }
 
   @Index()
-  @IsEmail({}, { message: "Incorrect email" })
+  @IsEmail(undefined, { message: "Incorrect email" })
   @IsNotEmpty({ message: "Заполните поле Email, please." })
   @Column({ unique: true })
   email: string;
 
   @Index()
-  @Length(3, 255, { message: "Username must be at least 3 characters long" })
+  @Length(3, 255, { message: "Must be at least 3 characters long" })
   @Column({ unique: true })
   username: string;
 
   @Exclude()
-  @Length(6, 255)
+  @Length(6, 255, { message: "Не меньше 6 символов." })
   @Column()
   password: string;
 
