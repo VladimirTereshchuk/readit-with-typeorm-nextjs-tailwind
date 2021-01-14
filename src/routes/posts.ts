eece4d1e230b3ed39b177ab96f8f6904f722fce8
VladimirTereshchuk <vofca1974@gmail.com>
2021-01-14 13:37:46 +0200

    added Comment entity and route for create comments

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,7 +1,7 @@
 import { Router, Request, Response } from "express";
+import Comment from "../entities/Comment";
 import Post from "../entities/Post";
 import Sub from "../entities/Sub";
-// import Sub from "../entities/Sub";
 
 import auth from "../middleware/auth";
 
@@ -46,10 +46,32 @@ const getPost = async (req: Request, res: Response) => {
   try {
     const post = await Post.findOneOrFail(
       { identifier, slug },
-      { relations: ["sub"] }
+      { relations: ["sub", "comments"] }
     );
 
     return res.json(post);
+  } catch (err) {
+    console.log(err);
+    return res.status(404).json({ error: "Post not found" });
+  }
+};
+
+const commentOnPost = async (req: Request, res: Response) => {
+  const { identifier, slug } = req.params;
+  const body = req.body.body;
+
+  try {
+    const post = await Post.findOneOrFail({ identifier, slug });
+
+    const comment = new Comment({
+      body,
+      user: res.locals.user,
+      post,
+    });
+
+    await comment.save();
+
+    return res.json(comment);
   } catch (err) {
     console.log(err);
     return res.status(404).json({ error: "Post not found" });
@@ -61,5 +83,6 @@ const router = Router();
 router.post("/", auth, createPost);
 router.get("/", getPosts);
 router.get("/:identifier/:slug", getPost);
+router.post("/:identifier/:slug/comments", auth, commentOnPost);
 
 export default router;
